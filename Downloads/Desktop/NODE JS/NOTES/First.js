9254ab5f62ffbaa1cef8c3a28aafef7e80e043ea
deepanshu-tyagi-11 <channutyagi553@gmail.com>
2026-06-11 00:25:59 +0530

    Delete Downloads/Desktop directory
    no

--- Downloads/Desktop/NODE JS/NOTES/First.js
+++
@@ -1,8 +0,0 @@
-console.log("Deepanshu is the best");
-
-
-const fs=require('fs'); // Importing the built-in 'fs' module
-fs.writeFile('output.txt', 'writing file', (err) => {      // file ka nam de dia output.txt
-    if (err) console.log ('error a gya');            // Error handling
-    console.log('file written successfully');         // Confirmation message
-});
