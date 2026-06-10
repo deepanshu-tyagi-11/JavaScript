869e4a2655ab8fd4e84a39f055a3a37c20b96ed4
deepanshu-tyagi-11 <deepanshutyagi3211@gmail.com>
2026-06-11 00:02:42 +0530

    add

---
+++ OneDrive/Desktop/JAVA SCRIPT/LEVEL4/naming convention.js
@@ -0,0 +1,20 @@
+//                                [NAMING CONVENTION ];
+
+// [NOTE]=>VARIABLE NAME ITS SHOULD CREAT ESAY TO UNDERSTAND .Mtlb haimna apna keyword ka nam ksha likhna hai.
+
+
+//1.)CAMEL CASE=>phla spelling mai sub kuch chota phir uska aga sabhi spelling ka phla word capital is called camel case.
+// => start with a lowercase letter capatlize the first letter of each subsequent word. 
+// SYNTAX=> myVariableName.
+
+// 2.)SNAKE CASE=>start with a lowercse letter seperate word with underscore .
+// syntax=>my_variable_name  .
+
+// 3.)KEBAB  CASE=>all lowercse letter seperate words with hypen used for htm and css not for JS.
+// syntax=>my-variable-name.
+//                              [WAY TO CREATE VARIABLE]
+//                              1.)var=> var ko bhr khai se bhe use kr sakta hai .basically var scope rule follow nhi krta.var does not follow scope..
+
+//                              2.)let=>  let ko hum bhr se use nhi krta sakta mtlb ya hamra scope ko follow krta hai. 
+
+                            //  3.)const=>  const ko hum chnage nhi kr sakta.
